0516cf1467d126c44a554da2c8a3b9721eda0db2
visalhmg <66855760+visalhmg@users.noreply.github.com>
2025-06-03 09:31:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "FdJxVZAR#WG_haAXgp_7O-sUb_6bcrRjVZP0u-YxedTz-32yC1cs" ,
 ALIVE_IMG:  process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Imvisal/PIRATE-MD-/refs/heads/main/file_0000000009606230ae71aac10d3d153b.png" ,
 ALIVE_MSG:  process.env.ALIVE_MSG || "Hello i am Alive now....! 🫣" ,
 };
